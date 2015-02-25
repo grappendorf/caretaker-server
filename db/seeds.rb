6f79e50042acaa84a9ff061f033fa798c3ba5686
Dirk Grappendorf <dirk@grappendorf.net>
2015-02-26 00:51:40 +0100

    Set the sample database user passwords equal to the user names

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 User.create!(name: 'admin',
              email: 'admin@example.com',
-             password: 'password',
-             password_confirmation: 'password').tap do |admin|
+             password: 'admin',
+             password_confirmation: 'admin').tap do |admin|
   admin.add_role :user
   admin.add_role :manager
   admin.add_role :admin
@@ -9,15 +9,15 @@ end
 
 User.create!(name: 'manager',
              email: 'manager@example.com',
-             password: 'password',
-             password_confirmation: 'password').tap do |manager|
+             password: 'manager',
+             password_confirmation: 'manager').tap do |manager|
   manager.add_role :user
   manager.add_role :manager
 end
 
 User.create!(name: 'user',
              email: 'user@example.com',
-             password: 'password',
-             password_confirmation: 'password').tap do |user|
+             password: 'user',
+             password_confirmation: 'user').tap do |user|
   user.add_role :user
 end
