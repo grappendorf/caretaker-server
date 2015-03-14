415dc2159f5376b4dc97c6a992ee120b7deb2770
Dirk Grappendorf <dirk@grappendorf.net>
2015-03-14 02:44:43 +0100

    fix: WLAN device connection handling

--- app/util/wlan_connection_state.rb
+++ app/util/wlan_connection_state.rb
@@ -2,24 +2,31 @@ module WlanConnectionState
 
   include ConnectionState
 
+  inject :scheduler
+
+  PING_TIMEOUT = 10 * 60
+
   # noinspection RubyArgCount
+  # (ambigious state method)
   def init_connection_state
     @connection_listeners = []
     device = self
 
-    # (ambigious state method)
     @machine = Statemachine.build do
       context device
       state :DISCONNECTED do
-        on_entry :notify_connection_listeners
-        event :connect, :WAIT_FOR_CONNECTION, :send_ping_request
+        on_entry :device_disconnected
+        event :connect, :WAIT_FOR_PING, :send_ping
+        event :ping, :CONNECTED
       end
-      state :WAIT_FOR_CONNECTION do
-        event :ping_response, :CONNECTED
+      state :WAIT_FOR_PING do
+        event :ping, :CONNECTED
+        event :timeout, :WAIT_FOR_PING, :send_ping
       end
       state :CONNECTED do
-        on_entry :notify_connection_listeners
-        event :disconnect, :DISCONNECTED
+        on_entry :device_connected
+        event :ping, :CONNECTED
+        event :timeout, :DISCONNECTED
       end
     end
   end
@@ -48,12 +55,24 @@ module WlanConnectionState
     @connection_listeners.each { |l| l.call self }
   end
 
-  def send_ping_request
-    send_message CaretakerMessages::PING_REQUEST
+  def send_ping
+    send_message CaretakerMessages::PING
   end
 
-  def on_ping_response
-    @machine.ping_response
+  def on_ping
+    @timeout_job.unschedule if @timeout_job
+    @timeout_job = scheduler.in PING_TIMEOUT do
+      @timeout_job = nil
+      @machine.timeout
+    end
+    @machine.ping
   end
 
+  def device_connected
+    notify_connection_listeners
+  end
+
+  def device_disconnected
+    notify_connection_listeners
+  end
 end
