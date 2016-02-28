a643cddec00ac3a66e15673dab46697a5cef0024
Dirk Grappendorf <dirk@grappendorf.net>
2016-02-28 17:26:43 +0100

    chore: pass name of the docker file as parameter to build-docker-image

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 VERSION=$(cat lib/version.rb |grep -o -P "(?<=VERSION = ')(.+)(?=')")
-docker build -t grappendorf/caretaker-server:${VERSION} .
+DOCKERFILE=${1:-Dockerfile}
+docker build -t grappendorf/caretaker-server:${VERSION} -f $DOCKERFILE .
