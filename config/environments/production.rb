faf70a185e299c5712a92d82904f5fee4ca4af96
Dirk Grappendorf <dirk@grappendorf.net>
2016-03-18 20:26:56 +0100

    chore: specify production config values as environment variables

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,9 +1,9 @@
 Application.configure do |config|
   config.log_level = Logger::Severity::WARN
   config.secret_key_base = ENV['SECRET_KEY_BASE']
-  config.jwt_expiration = 24.hours
+  config.jwt_expiration = ENV['JWT_EXPIRATION'] || 24.hours
   config.public_ip = ENV['PUBLIC_IP']
-  config.broadcast_port = 55555
-  config.server_port = 2000
-  config.device_port = 2000
+  config.broadcast_port = ENV['BROADCAST_PORT'] || 55555
+  config.server_port = ENV['SERVER_PORT'] || 2000
+  config.device_port = ENV['DEVICE_PORT'] || 2000
 end
